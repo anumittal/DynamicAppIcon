b38cb940e36f076e03b235efae8daf7caea0e298
Anu Mittal <anu@circles.asia>
2019-12-09 12:57:59 +0800

    Initial Commit

---
+++ DynamicAppIcon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicAppIcon
+//
+//  Created by Anu Mittal on 9/12/19.
+//  Copyright © 2019 Anu Mittal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
